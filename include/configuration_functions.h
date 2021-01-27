7e5bc46588fb80328aa32a563fb33dfcac3ba739
Michael Barskikh <barskikh@tecon.ru>
2021-01-27 16:35:57 +0300

    EEPROM_ADDRESSES converted from class to struct.

--- include/configuration_functions.h
+++ include/configuration_functions.h
@@ -5,17 +5,18 @@
 #include "functions.h"
 #include "eeprom_function.h"
 
-class EEPROM_ADDRESSES
+struct EEPROM_ADDRESSES
 {
-private:
-public:
-    static const uint16_t version = 0x0;
-    static const uint16_t name    = 0x2;
-    static const uint16_t state   = 0xB;
-    static const uint16_t effect  = 0xC;
-    static const uint16_t alarms  = 0x14;
+    enum ADDRESS_MAP : uint16_t
+    {
+        version = 0x0,
+        name    = 0x2,
+        state   = 0xB,
+        effect  = 0xC,
+        alarms  = 0x14
+    };
 
-    enum EFFECT_OFFSETS: uint16_t
+    enum EFFECT_OFFSETS : uint16_t
     {
         effect_number      = 0x0,
         effect_brightness  = 0x1,
@@ -24,7 +25,7 @@ public:
         EFFECT_SIZE        = 0x8
     };
 
-    enum ALARM_OFFSETS: uint16_t
+    enum ALARM_OFFSETS : uint16_t
     {
         alarm_state = 0x0,
         alarm_hour,
