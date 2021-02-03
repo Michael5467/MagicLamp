e476100a9615e53e50d0eb9692b0c460a438f2cc
Michael Barskikh <barskikh@tecon.ru>
2021-02-03 16:13:21 +0300

    Returned the comments' alignment

--- include/functions.h
+++ include/functions.h
@@ -95,7 +95,7 @@ struct lamp_config_s
 #pragma pack(pop)
 
 // Functions
-// work API
+    // work API
 void changePower(the_lamp_state_t *lamp_state);
 void SelectEffect(the_lamp_state_t *lamp_state);
 void ResetEffectSettings(the_lamp_state_t *lamp_state);
@@ -111,7 +111,7 @@ void setEffectsSpeed(the_lamp_state_t *lamp_state, uint32_t Value);
 void changeEffectsSpeed(the_lamp_state_t *lamp_state, int8_t ChangeValue);
 void convertRAW(the_lamp_state_t *lamp_state);
 
-// utility functions declaration
+    // utility functions declaration
 void printDecNum(uint32_t num);
 void printDateTimeStruct(local_date_time_t *date_time);
 void printTime(time_t currentLocalTime);
@@ -121,39 +121,39 @@ uint8_t convert_from_ISO8601(uint8_t ISO8601Day);
 uint32_t str2int(char *str, uint8_t len);
 String lamp_state_2_string(the_lamp_state_t *lamp_state);
 uint16_t ChangeParameterValue(uint16_t value, int8_t delta, uint16_t limit = 255, boolean saturation = true); // Change the parameter's value by signed delta
-                                                                                                              // Matrix filling
+        // Matrix filling
 void fillAllbyColor(CRGB *leds, CRGB color);                                                                  // Fill the matrix by color
 void fillRandomly(CRGB *leds, uint16_t amount);                                                               // Fill the matrix with random points by random color
 void fillRandomlyAll(CRGB *leds);                                                                             // Fill the matrix by random colors
-                                                                                                              // Matrix-to-strip coordinates conversion
+        // Matrix-to-strip coordinates conversion
 uint16_t getPixelNumber(uint8_t x, uint8_t y);                                                                // Get the pixel number in the tape by coordinates
-                                                                                                              // Operation with pixel's color
+        // Operation with pixel's color
 void drawPixel(CRGB *leds, uint16_t index, CRGB color);                                                       // Set color of the pixel 'in strip' by index
 void drawPixelXY(CRGB *leds, uint8_t x, uint8_t y, CRGB color);                                               // Set color of the pixel 'in matrix' by X,Y coordinates
 uint32_t getPixelColor(CRGB *leds, uint16_t index);                                                           // Get color of the pixel 'in strip' by index
 uint32_t getPixelColorXY(CRGB *leds, uint8_t x, uint8_t y);                                                   // Get color of the pixel 'in strip' by X,Y coordinates
-                                                                                                              // Colors' attenuation
+        // Colors' attenuation
 void fadePixel(CRGB *leds, uint8_t x, uint8_t y, uint8_t step);                                               // Attenuation of the pixel: check the current pixel's brightness and reduce it; calls fadeToBlackBy function from fastLED library
 void fadeMatrix(CRGB *leds, uint8_t step);                                                                    // Attenuation of all pixels in matrix
 void fadePixelManually(CRGB *leds, uint8_t x, uint8_t y, uint8_t step);                                       // Attenuation of the pixel: check the current pixel's brightness and reduce it
 void fadeMatrixManually(CRGB *leds, uint8_t step);
-// Shift/rotate matrix
+        // Shift/rotate matrix
 void shiftMatrixDown(CRGB *leds);      // Shift matrix down  on 1 by Y
 void shiftMatrixUp(CRGB *leds);        // Shift matrix up    on 1 by Y
 void shiftMatrixRight(CRGB *leds);     // Shift matrix right on 1 by X
 void shiftMatrixLeft(CRGB *leds);      // Shift matrix left  on 1 by X
 void shiftMatrixDownRight(CRGB *leds); // Shift matrix down-right on 1 by X an dY
 
-// Time
+    // Time
 boolean NTP_Synchronization(NTPClient *timeClient, local_date_time_t *date_time);
 boolean CheckInternetAccess();
 
-// Effects
-// Three auxiliary functions for fire
+    // Effects
+        // Three auxiliary functions for fire
 void generateLine();
 void shiftUp();
 void drawFrame(CRGB *leds, int pcnt);
-// Main functions
+        // Main functions
 void snowRoutine(the_lamp_state_t *lamp_state);
 void ballsRoutine(the_lamp_state_t *lamp_state);
 void fireRoutine(the_lamp_state_t *lamp_state);
@@ -161,15 +161,15 @@ void sparklesRoutine(the_lamp_state_t *lamp_state);
 void matrixRoutine(the_lamp_state_t *lamp_state);
 void starfallRoutine(the_lamp_state_t *lamp_state);
 
-// fullscreen effects
-// Three auxiliary functions for fullscreen effects
+    // fullscreen effects
+        // Three auxiliary functions for fullscreen effects
 void SetupRandomPalette();
 void SetupBlackAndWhiteStripedPalette();
 void SetupPurpleAndGreenPalette();
 void fillNoiseLED(CRGB *leds);
 void fillnoise8();
 void mapNoiseToLEDsUsingPalette(CRGB *leds);
-// Main functions
+        // Main functions
 void lavaNoise(the_lamp_state_t *lamp_state);
 void cloudNoise(the_lamp_state_t *lamp_state);
 void zebraNoise(the_lamp_state_t *lamp_state);
@@ -181,8 +181,8 @@ void randomNoise(the_lamp_state_t *lamp_state);
 void rainbowNoise(the_lamp_state_t *lamp_state);
 void rainbowStripeNoise(the_lamp_state_t *lamp_state);
 
-// Games
-// Snake
+    // Games
+        // Snake
 void newSnake(CRGB *leds);
 void newApple(CRGB *leds);
 void newGameSnake(CRGB *leds);
